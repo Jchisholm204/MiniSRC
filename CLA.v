99c0be4fca4df4953940763c6d4d9fd87c2f51a1
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-01-23 09:44:30 -0500

    cla

--- CLA.v
+++ CLA.v
@@ -7,7 +7,7 @@ module CLA(
     oCarry,
     oOverflow,
     oZero,
-    oNegative,
+    oNegative
 );
 
 input wire iCarry;
@@ -29,3 +29,21 @@ assign oZero = ~|oS;
 assign oNegative = oS[31];
 
 endmodule
+
+input wire iCarry;
+input wire [31:0] iX, iY;
+output wire [31:0] oS;
+output wire oCarry, oOverflow, oZero, oNegative;
+
+wire [31:0] G, P;
+wire [32:0] C;
+
+assign G = iX & iY;
+assign P = iX | iY;
+assign C = {G | (P & C[31:0]), iCarry};
+assign oS = iX ^ iY ^ C[31:0];
+
+assign oCarry = C[32];
+assign oOverflow = C[32] ^ C[31];
+assign oZero = ~|oS;
+assign oNegative = oS[31];
