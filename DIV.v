95c6d3565a53653b2052a4a35913f2590ea1e6e9
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2025-01-26 00:25:30 -0500

    simple 4b divider working

--- DIV.v
+++ DIV.v
@@ -1,117 +1,122 @@
 module DIV(
-    iA, iB, oQ, oR
+    iClk, nRst,
+    iQ, iM,
+    oQ, oR
 );
 
-input wire [3:0] iA, iB;
+input wire iClk, nRst;
+input wire [3:0] iQ, iM;
 output wire [3:0] oQ, oR;
 
+reg [3:0] A = 4'd0, Q = 4'd0, q = 4'd0;
+reg [3:0] step = 4'd0;
 
-wire [6:0] div = {iA, 3'b000};
-wire [3:0] res;
+assign oQ = Q;
+assign oR = A;
 
-wire [3:0] bout1, bout2, bout3, bout4;
-wire [3:0] sum1, sum2, sum3, sum4;
+always @(posedge iClk or negedge nRst) begin
+    if(!nRst) begin
+        A = 4'd0;
+        step = 4'd0;
+        Q = 4'd0;
+        q = iQ;
+    end
+    else if(step == 4'd0) begin
+        if(A[3]) begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
+            A = A + iM;
+            // Q = {Q[3:0], 1'b0};
+        end
+        else begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
+            A = A - iM;
+            // Q = {Q[3:0], 1'b1};
+        end
+        step = step + 4'd1;
+    end
+    else if(step == 4'd1) begin
+        if(A[3]) begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
-assign oQ = res;
-assign oR = res[0] ? sum4 : sum4 + bout4;
+            A = A + iM;
+            Q = {Q[3:0], 1'b0};
+        end
+        else begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
-DIV_LEVEL l1(
-    .iA(div[6:3]),
-    .iB(iB),
-    .iMode(1'b1),
-    .oQ(res[3]),
-    .oR(sum1),
-    .oB(bout1)
-);
+            A = A - iM;
+            Q = {Q[3:0], 1'b1};
+        end
+        step = step + 4'd1;
+    end
+    else if(step == 4'd2) begin
+        if(A[3]) begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
-DIV_LEVEL l2(
-    .iA({sum1[3:1], div[2]}),
-    .iB(bout1),
-    .iMode(res[3]),
-    .oQ(res[2]),
-    .oR(sum2),
-    .oB(bout2)
-);
+            A = A + iM;
+            Q = {Q[3:0], 1'b0};
+        end
+        else begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
-DIV_LEVEL l3(
-    .iA({sum2[3:1], div[1]}),
-    .iB(bout2),
-    .iMode(res[2]),
-    .oQ(res[1]),
-    .oR(sum3),
-    .oB(bout3)
-);
+            A = A - iM;
+            Q = {Q[3:0], 1'b1};
+        end
+        step = step + 4'd1;
+    end
+    else if(step == 4'd3) begin
+        if(A[3]) begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
-DIV_LEVEL l4(
-    .iA({sum3[3:1], div[0]}),
-    .iB(bout2),
-    .iMode(res[1]),
-    .oQ(res[0]),
-    .oR(sum4),
-    .oB(bout4)
-);
+            A = A + iM;
+            Q = {Q[3:0], 1'b0};
+        end
+        else begin
+            A = {A[2:0], q[3]};
+            q = {q[2:0], 1'b0};
 
+            A = A - iM;
+            Q = {Q[3:0], 1'b1};
+        end
+        step = step + 4'd1;
+    end
+    // else if(step == 4'd4) begin
+    //     if(A[3]) begin
+    //         A = {A[2:0], q[3]};
+    //         q = {q[2:0], 1'b0};
 
-endmodule
+    //         A = A + iM;
+    //         Q = {Q[3:0], 1'b0};
+    //     end
+    //     else begin
+    //         A = {A[2:0], q[3]};
+    //         q = {q[2:0], 1'b0};
 
-module DIV_LEVEL(
-    iA, iB,
-    iMode,
-    oQ, oR, oB
-);
+    //         A = A - iM;
+    //         Q = {Q[3:0], 1'b1};
+    //     end
+    //     step = step + 4'd1;
+    // end
+    else if(step == 4'd4) begin
+        if(A[3]) begin
+            A = A + iM;
+            Q = {Q[3:0], 1'b0};
+        end
+        else begin
+            Q = {Q[3:0], 1'b1};
+        end
+        step = step + 4'd1;
+    end
+    // else begin
 
-input wire [3:0] iA, iB;
-input wire iMode;
-output wire [3:0] oR;
-output wire oQ;
-output wire [3:0] oB;
+end
 
-wire [4:0] mode;
-assign mode[0] = iMode;
-wire [3:0] cout;
-
-DIV_CELL i1(
-    .iA(iA[3]),
-    .iB(iB[3]),
-    .iMode(mode[0]),
-    .oMode(mode[1]),
-    .iCarry(cout[1]),
-    .oCarry(cout[0]),
-    .oSum(oR[0]),
-    .oB(oB[0])
-);
-
-DIV_CELL i2(
-    .iA(iA[2]),
-    .iB(iB[2]),
-    .iMode(mode[1]),
-    .oMode(mode[2]),
-    .iCarry(cout[2]),
-    .oCarry(cout[1]),
-    .oSum(oR[1]),
-    .oB(oB[1])
-);
-
-DIV_CELL i3(
-    .iA(iA[1]),
-    .iB(iB[1]),
-    .iMode(mode[2]),
-    .oMode(mode[3]),
-    .iCarry(cout[3]),
-    .oCarry(cout[2]),
-    .oSum(oR[2]),
-    .oB(oB[2])
-);
-
-DIV_CELL i4(
-    .iA(iA[0]),
-    .iB(iB[0]),
-    .iMode(mode[3]),
-    .oMode(mode[4]),
-    .iCarry(mode[4]),
-    .oCarry(cout[3]),
-    .oSum(oR[3]),
-    .oB(oB[3])
-);
 
 endmodule
--- tb/sim_DIV.v
+++ tb/sim_DIV.v
@@ -1,25 +1,55 @@
-`timescale 1ps/1ps
+`timescale 1ms/1ps
 module sim_DIV();
 
 reg [3:0] Q, D;
 wire [3:0] rez, rmdr;
 
+wire Clk;
+wire nRst;
+reg regnRst = 1'b1;
+reg divrst = 1'b0;
+
+assign nRst = regnRst;
+
+ClockGenerator #( // Generic Constants
+    .ClockFrequency(200e3)) 
+     cg1 ( // Port Assignments
+    .nRst(nRst), 
+    .oClk(Clk)
+);
+
 DIV divider(
-    .iA(Q),
-    .iB(D),
+    .iClk(Clk),
+    .nRst(divrst),
+    .iQ(Q),
+    .iM(D),
     .oQ(rez),
     .oR(rmdr)
 );
 
 initial begin
-    Q = 4'd5;
-    D = 4'd1;
-    #10
+    // #20 regnRst = 1'b1;
+    // #20 regnRst = 1'b0;
+    divrst = 1'b0;
+    Q = 4'd7;
+    D = 4'd3;
+    #1
+    divrst = 1'b1;
+    #1
     Q = 4'd4;
     D = 4'd2;
-    #10
+    #1
+    divrst = 1'b0;
+    #1
+    divrst = 1'b1;
+    #1
+    divrst = 1'b0;
     Q = 4'd3;
     D = 4'd3;
+    #1
+    divrst = 1'b1;
+    #100
+    divrst = 1'b1;
 end
 
 
