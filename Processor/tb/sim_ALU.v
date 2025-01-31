ebf94ece44e4b9a6117862b26baf280de1dcf8f4
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2025-01-31 13:10:28 -0500

    alu testing complete

--- Processor/tb/sim_ALU.v
+++ Processor/tb/sim_ALU.v
@@ -15,50 +15,87 @@ ALU alu(
     .oNeg(neg)
 );
 
+integer i;
+integer j;
 initial begin
     // Add Testing
     Ctrl = alu.ALUC_ADD;
-    A = 32'd5;
-    B = 32'd5;
-    // Res = 10
-    #1
-    A = 32'd12;
-    B = 32'd400;
-    // Rez = 412
-    #1
-    A = -32'd5;
-    B = 32'd5;
-    // Rez = 0
-    #1
-    A = -32'd5;
-    B = -32'd5;
-    // Rez = -10
-    #1
-    A = 32'd33;
-    B = -32'd3;
-    // Rez = -10
-    #1
-    // SUB Testing
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_lo != (A+B))
+                $display ("Add Failure %0d + %0d = %0d", A, B, C_lo);
+            // else
+            //     $display ("Add Success %0d + %0d = %0d", A, B, C_lo);
+        end
+    end
+    // Subtraction Testing
     Ctrl = alu.ALUC_SUB;
-    A = 32'd5;
-    B = 32'd5;
-    // Res = 0
-    #1
-    A = 32'd12;
-    B = 32'd400;
-    // Rez = -388
-    #1
-    A = -32'd5;
-    B = 32'd5;
-    // Rez = -10
-    #1
-    A = -32'd5;
-    B = -32'd5;
-    // Rez = 0
-    #1
-    A = 32'd33;
-    B = -32'd3;
-    // Rez = -36
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_lo != (A-B))
+                $display ("SUB Failure %0d + %0d = %0d", A, B, C_lo);
+        end
+    end
+    // OR Test
+    Ctrl = alu.ALUC_OR;
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_lo != (A | B))
+                $display ("OR Failure %0d + %0d = %0d", A, B, C_lo);
+        end
+    end
+    // XOR  Test
+    Ctrl = alu.ALUC_XOR;
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_lo != (A ^ B))
+                $display ("XOR Failure %0d + %0d = %0d", A, B, C_lo);
+        end
+    end
+    // AND  Test
+    Ctrl = alu.ALUC_AND;
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_lo != (A & B))
+                $display ("AND Failure %0d + %0d = %0d", A, B, C_lo);
+        end
+    end
+    // DIV  Test
+    Ctrl = alu.ALUC_DIV;
+    for(i = 0; i < 31; i = i + 1) begin
+        A = 32'b1 << i;  // Set only the i-th bit
+        for(j = 0; j < 31; j = j + 1) begin
+            B = 32'b1 << j;  // Set only the i-th bit
+            #1
+            if(C_hi != (A / B) || C_lo != (A % B))
+                $display ("DIV Failure %0d / %0d = %0d %0d", A, B, C_hi, C_lo);
+        end
+    end
+    for(i = 1000; i < 5000; i = i + 1) begin
+        A = -i;
+        for(j = 0; j < 1000; j = j + 1) begin
+            B = 500 - j;
+            #1
+            // Must verify manually - negative division has issues in v
+            // if(C_hi != (A / B) || C_lo != (A % B))
+            //     $display ("DIV Failure %0d / %0d = %0d %0d", A, B, C_hi, C_lo);
+        end
+    end
 end
 
 endmodule
