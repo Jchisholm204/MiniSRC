5a212674f74bccef21ba53e3f5af3389e0bf5cbf
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2025-01-31 15:46:31 -0500

    add ALU to processor

--- Processor/Processor.v
+++ Processor/Processor.v
@@ -13,12 +13,18 @@ output wire [31:0] oMemData, oMemAddr;
 wire Clk;
 assign Clk = iClk & ~nRst;
 
-// Register File
+// Register File IO
 wire RF_nRst, RF_iWrite;
 wire [3:0] RF_iAddrA, RF_iAddrB, RF_iAddrC;
 wire [31:0] RF_oRegA, RF_oRegB, RF_iRegC;
 
-// Control Unit
+// ALU IO
+wire [31:0] ALU_iA, ALU_iB, ALU_oC_hi, ALU_oC_lo;
+wire [4:0]  ALU_iCtrl;
+wire ALU_oZero, ALU_oNeg;
+
+// Program Counter
+
 
 // Register File
 RegFile RF(
@@ -34,6 +40,15 @@ RegFile RF(
 );
 
 // ALU
+ALU alu(
+    .iA(ALU_iA),
+    .iB(ALU_iB),
+    .iCtrl(ALU_iCtrl),
+    .oC_hi(ALU_oC_hi),
+    .oC_lo(ALU_oC_lo),
+    .oZero(ALU_oZero),
+    .oNeg(ALU_oNeg)
+);
 
 // Memory
 
