2d7c8c85bbebd0a5ffa07cb80525a4c954c03440
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-02-04 12:36:08 -0500

    proc sim wip

--- Processor/tb/sim_processor.v
+++ Processor/tb/sim_processor.v
@@ -1,13 +1,30 @@
 `timescale 1ns/1ps
+`include "../Control/ISA.vh"
 
 module sim_PROC();
+
 wire Clk;
-ClockGenerator(
+
+ClockGenerator cg(
     .nRst(1'b1),
     .oClk(Clk)
 );
 
-Processor
+reg proc_reset = 1'b0;
+wire mem_read, mem_write;
+wire [31:0] proc_mem_out, proc_mem_addr;
+reg [31:0] proc_mem_in = {`ISA_ADDI, 27'd0};
+
+Processor proc(
+    .iClk(Clk),
+    .nRst(proc_reset),
+    .oMemAddr(proc_mem_addr),
+    .oMemData(proc_mem_out),
+    .iMemData(proc_mem_in),
+    .iMemRdy(1'b1),
+    .oMemRead(mem_read),
+    .oMemWrite(mem_write)
+);
 
 
 endmodule
