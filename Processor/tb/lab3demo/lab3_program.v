46e99a48b63a146c065403ebe6455a481588d907
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2025-03-12 14:40:05 -0400

    added a couple more instructions to the lab3 program to demo. probably a good time now to use the assembler to generate the code.

--- Processor/tb/lab3demo/lab3_program.v
+++ Processor/tb/lab3demo/lab3_program.v
@@ -44,7 +44,46 @@ initial begin
 
     // TODO: Initialize Instruction Memory.
     // Encode your program in memory with the starting address zero
-    mem[8'h0] = `INS_I(`ISA_ADDI, 4'd3, 4'd0, 19'h65);
+    mem[8'h00] = `INS_I(`ISA_ADDI, 4'd3, 4'd0, 19'h65); // ldi R3, 0x65 ; R3 = 0x65 // addi r3, r0, 0x65
+    mem[8'h01] = `INS_I(`ISA_ADDI, 4'd3, 4'd3, 19'h3);  // ldi R3, 3(R3) ; R3 = 0x68 // addi R3, R3, 3
+    mem[8'h02] = `INS_I(`ISA_LD, 4'd2, 4'd0, 19'h54);   // ld R2, 0x54 ; R2 = (0x54) = 0x97
+    // ldi R2, 1(R2) ; R2 = 0x98 
+    // ld R0, -6(R2) ; R0 = (0x92) = 0x46 
+    // ldi R1, 3 ; R1 = 3 
+    // ldi R3, 0x57 ; R3 = 0x57 
+    // brmi R3, 3 ; continue with the next instruction (will not branch) 
+    // ldi R3, 3(R3) ; R3 = 0x5A 
+    // ld R4, -6(R3) ; R4 = (0x5A - 6) = 0x97 
+    // nop brpl R4, 2 ; continue with the instruction at “target” (will branch) 
+    // ldi R6, 7(R3) ; this instruction will not execute 
+    // ldi R5, -4(R6) ; this instruction will not execute 
+    // add R3, R3, R1 ; R3 = 0x5D 
+    // addi R4, R4, 2 ; R4 = 0x99 
+    // neg R4, R4 ; R4 = 0xFFFFFF67 
+    // not R4, R4 ; R4 = 0x98 
+    // andi R4, R4, 0xF ; R4 = 8 
+    // ror R2, R0, R1 ; R2 = 0xC0000008 
+    // ori R4, R2, 7 ; R4 = 0xC000000F 
+    // shra R2, R4, R1 ; R2 = 0xF8000001 
+    // shr R3, R3, R1 ; R3 = 0xB 
+    // st 0x92, R3 ; (0x92) = 0xB new value in memory with address 0x92 
+    // rol R3, R0, R1 ; R3 = 0x230 
+    // or R5, R1, R0 ; R5 = 0x47 
+    // and R2, R3, R0 ; R2 = 0 
+    // st 0x54(R2), R5 ; (0x54) = 0x47 new value in memory with address 0x54 
+    // sub R0, R3, R5 ; R0 = 0x1E9 
+    // shl R2, R3, R1 ; R2 = 0x1180 
+    // ldi R5, 8 ; R5 = 8 
+    // ldi R6, 0x17 ; R6 = 0x17 
+    // mul R6, R5 ; HI = 0; LO = 0xB8 
+    // mfhi R4 ; R4 = 0 
+    // mflo R7 ; R7 = 0xB8 
+    // div R6, R5 ; HI = 7 , LO = 2 
+    // ldi R10, 1(R5) ; R10 = 9 setting up argument registers 
+    // ldi R11, -3(R6) ; R11 = 0x14 R10, R11, R12, 
+    // and R13 ldi R12, 1(R7) ; R12 = 0xB9 
+    // ldi R13, 4(R4) ; R13 = 4 jal R12 ; address of subroutine subA in R12 - return address in R8 
+    // halt ; upon return, the program halts 
 
     #1
     // Initialize registers R0 - R15 and the PC to 0 with the Reset input signal. 
@@ -66,4 +105,3 @@ always @(mem_read, mem_write) begin
 end
 
 endmodule
-
